e4073ba900f2378fcb6e1d79fd7921c5a87dcb91
jghaanstra <jghaanstra@gmail.com>
2018-09-08 15:19:51 +0200

    Release 1.1.0

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ class ShellyApp extends Homey.App {
   onInit() {
     this.log('Initializing Shelly App ...');
 
+    // SHELLY 1
     new Homey.FlowCardAction('flipbackSwitch')
       .register()
       .registerRunListener((args, state) => {
@@ -18,6 +19,67 @@ class ShellyApp extends Homey.App {
         }
         return Promise.resolve(args.switch);
       })
+
+    // SHELLY 2
+    new Homey.FlowCardCondition('relay0Powered')
+      .register()
+      .registerRunListener((args, state) => {
+        if (args.device.getCapabilityValue('onoff.relay0')) {
+          return Promise.resolve(true);
+        } else {
+          return Promise.reject(false);
+        }
+      })
+
+    new Homey.FlowCardCondition('relay1Powered')
+      .register()
+      .registerRunListener((args, state) => {
+        if (args.device.getCapabilityValue('onoff.relay1')) {
+          return Promise.resolve(true);
+        } else {
+          return Promise.reject(false);
+        }
+      })
+
+    new Homey.FlowCardAction('relay0OnAction')
+      .register()
+      .registerRunListener((args, state) => {
+        util.sendCommand('/relay/0?turn=on', args.device.getSetting('address'), args.device.getSetting('username'), args.device.getSetting('password'));
+        return Promise.resolve(args.switch);
+      })
+
+    new Homey.FlowCardAction('relay1OnAction')
+      .register()
+      .registerRunListener((args, state) => {
+        util.sendCommand('/relay/1?turn=on', args.device.getSetting('address'), args.device.getSetting('username'), args.device.getSetting('password'));
+        return Promise.resolve(args.switch);
+      })
+
+    new Homey.FlowCardAction('relay0OffAction')
+      .register()
+      .registerRunListener((args, state) => {
+        util.sendCommand('/relay/0?turn=off', args.device.getSetting('address'), args.device.getSetting('username'), args.device.getSetting('password'));
+        return Promise.resolve(args.switch);
+      })
+
+    new Homey.FlowCardAction('relay1OffAction')
+      .register()
+      .registerRunListener((args, state) => {
+        util.sendCommand('/relay/1?turn=off', args.device.getSetting('address'), args.device.getSetting('username'), args.device.getSetting('password'));
+        return Promise.resolve(args.switch);
+      })
+
+    new Homey.FlowCardAction('flipbackSwitch2')
+      .register()
+      .registerRunListener((args, state) => {
+        if (args.switch === '1') {
+          util.sendCommand('/relay/'+ args.relay +'?turn=on&timer='+ args.timer +'', args.device.getSetting('address'), args.device.getSetting('username'), args.device.getSetting('password'));
+        } else {
+          util.sendCommand('/relay/'+ args.relay +'?turn=off&timer='+ args.timer +'', args.device.getSetting('address'), args.device.getSetting('username'), args.device.getSetting('password'));
+        }
+        return Promise.resolve(args.switch);
+      })
+
   }
 
 }
