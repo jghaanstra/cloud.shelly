ee9b123c728f27c0fc189545155f75bbc99a6874
jghaanstra <jghaanstra@gmail.com>
2019-09-01 23:12:05 +0200

    Release 1.11.0

--- APPSTORE.md
+++ APPSTORE.md
@@ -10,6 +10,7 @@ This Homey app allows you to control Shelly devices. The supported Shelly device
 * Shelly Plug S
 * Shelly 4 Pro
 * Shelly RBGW2 (Color, 4x White and 2x CW/WW mode)
+* Shelly EM
 
 ## Instructions
 Add your Shelly device in Homey just like you would add any other device. Enter the IP address and the polling frequency in the pairing wizard. If you have set a username and password to access your Shelly device you need to enter these during pairing as well. Connect the device and you are good to go.
@@ -18,5 +19,5 @@ Add your Shelly device in Homey just like you would add any other device. Enter 
 For support please use the official support topic on the forum [here](https://community.athom.com/t/765).
 
 ## Changelog
-### v1.10.0- 2019-08-11
-* IMPROVEMENT: added device discovery in pair wizard
+### v1.11.0- 2019-09-xx
+* NEW: add support for Shelly EM
---
+++ drivers/shellyem/device.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const Homey = require('homey');
+const util = require('/lib/util.js');
+
+class ShellyEmDevice extends Homey.Device {
+
+  onInit() {
+    var interval = this.getSetting('polling') || 5;
+    this.pollDevice(interval);
+    this.setAvailable();
+
+    // LISTENERS FOR UPDATING CAPABILITIES
+    this.registerCapabilityListener('onoff', (value, opts) => {
+      if (value) {
+        return util.sendCommand('/relay/0?turn=on', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'));
+      } else {
+        return util.sendCommand('/relay/0?turn=off', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'));
+      }
+    });
+  }
+
+  onDeleted() {
+    clearInterval(this.pollingInterval);
+  }
+
+  // HELPER FUNCTIONS
+  pollDevice(interval) {
+    clearInterval(this.pollingInterval);
+    clearInterval(this.pingInterval);
+
+    this.pollingInterval = setInterval(() => {
+      util.sendCommand('/status', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'))
+        .then(result => {
+          let channel = this.getStoreValue('channel');
+          var state = result.relays[0].ison; // check
+          var power = result.emeters[channel].power; // check
+          var reactive_power = result.emeters[channel].reactive; // check
+          var voltage = result.emeters[channel].voltage; // check
+          var total_consumed = result.emeters[channel].total; // check
+          var total_returned = result.emeters[channel].total_returned;
+
+          // capability onoff
+          if (state != this.getCapabilityValue('onoff')) {
+            this.setCapabilityValue('onoff', state);
+          }
+
+          // capability measure_power
+          if (power != this.getCapabilityValue('measure_power')) {
+            this.setCapabilityValue('measure_power', power);
+          }
+
+          // capability reactive_power
+          if (reactive_power != this.getCapabilityValue('reactive_power')) {
+            this.setCapabilityValue('reactive_power', reactive_power);
+          }
+
+          // capability measure_voltage
+          if (voltage != this.getCapabilityValue('measure_voltage')) {
+            this.setCapabilityValue('measure_voltage', voltage);
+          }
+
+          // capability meter_power_consumed
+          if (total_consumed != this.getCapabilityValue('meter_power_consumed')) {
+            this.setCapabilityValue('meter_power_consumed', total_consumed);
+          }
+
+          // capability meter_power_returned
+          if (total_returned != this.getCapabilityValue('meter_power_returned')) {
+            this.setCapabilityValue('meter_power_returned', total_returned);
+          }
+
+        })
+        .catch(error => {
+          this.log(error);
+          this.setUnavailable(Homey.__('Unreachable'));
+          this.pingDevice();
+        })
+    }, 1000 * interval);
+  }
+
+  pingDevice() {
+    clearInterval(this.pollingInterval);
+    clearInterval(this.pingInterval);
+
+    this.pingInterval = setInterval(() => {
+      util.sendCommand('/status', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'))
+        .then(result => {
+          this.setAvailable();
+          var interval = this.getSetting('polling') || 5;
+          this.pollDevice(interval);
+        })
+        .catch(error => {
+          this.log('Device is not reachable, pinging every 63 seconds to see if it comes online again.');
+        })
+    }, 63000);
+  }
+
+}
+
+module.exports = ShellyEmDevice;
