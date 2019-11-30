6fc29276e11c4a672919cdeae26d5823a65e9e34
jghaanstra <jghaanstra@gmail.com>
2019-11-30 13:08:59 +0100

    Release 1.14.0

---
+++ drivers/shellydimmer/device.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const Homey = require('homey');
+const util = require('/lib/util.js');
+
+class ShellyDimmerDevice extends Homey.Device {
+
+  onInit() {
+    var interval = this.getSetting('polling') || 5;
+    this.pollDevice(interval);
+    this.setAvailable();
+
+    // LISTENERS FOR UPDATING CAPABILITIES
+    this.registerCapabilityListener('onoff', (value, opts) => {
+      if (value) {
+        return util.sendCommand('/light/0?turn=on', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'));
+      } else {
+        return util.sendCommand('/light/0?turn=off', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'));
+      }
+    });
+
+    this.registerCapabilityListener('dim', (value, opts) => {
+      if (!this.getCapabilityValue('onoff')) {
+        this.setCapabilityValue('onoff', true);
+      }
+      var dim = value * 100;
+      return util.sendCommand('/light/0?brightness='+ dim +'', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'));
+    });
+  }
+
+  onDeleted() {
+    clearInterval(this.pollingInterval);
+  }
+
+  // HELPER FUNCTIONS
+  pollDevice(interval) {
+    clearInterval(this.pollingInterval);
+    clearInterval(this.pingInterval);
+
+    this.pollingInterval = setInterval(() => {
+      util.sendCommand('/status', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'))
+        .then(result => {
+          let state = result.lights[0].ison;
+          let power = result.meters[0].power;
+          let total_consumption = result.meters[0].total;
+          let meter_power = total_consumption * 0.000017;
+          let dim = Number(result.lights[0].brightness) / 100;
+          let temperature = result.tmp.tC;
+
+          // capability onoff
+          if (state != this.getCapabilityValue('onoff')) {
+            this.setCapabilityValue('onoff', state);
+          }
+
+          // capability measure_power
+          if (power != this.getCapabilityValue('measure_power')) {
+            this.setCapabilityValue('measure_power', power);
+          }
+
+          // capability meter_power_wmin
+          if (total_consumption != this.getCapabilityValue('meter_power_wmin')) {
+            this.setCapabilityValue('meter_power_wmin', total_consumption);
+          }
+
+          // capability meter_power
+          if (meter_power != this.getCapabilityValue('meter_power')) {
+            this.setCapabilityValue('meter_power', meter_power);
+          }
+
+          // capability dim
+          if (dim != this.getCapabilityValue('dim')) {
+            this.setCapabilityValue('dim', dim);
+          }
+
+          // capability measure_temperature
+          if (temperature != this.getCapabilityValue('measure_temperature')) {
+            this.setCapabilityValue('measure_temperature', temperature);
+          }
+
+        })
+        .catch(error => {
+          this.log(error);
+          this.setUnavailable(Homey.__('Unreachable'));
+          this.pingDevice();
+        })
+    }, 1000 * interval);
+  }
+
+  pingDevice() {
+    clearInterval(this.pollingInterval);
+    clearInterval(this.pingInterval);
+
+    this.pingInterval = setInterval(() => {
+      util.sendCommand('/status', this.getSetting('address'), this.getSetting('username'), this.getSetting('password'))
+        .then(result => {
+          this.setAvailable();
+          var interval = this.getSetting('polling') || 5;
+          this.pollDevice(interval);
+        })
+        .catch(error => {
+          this.log('Device is not reachable, pinging every 63 seconds to see if it comes online again.');
+        })
+    }, 63000);
+  }
+
+}
+
+module.exports = ShellyDimmerDevice;
