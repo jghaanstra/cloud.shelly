61c99e6c4abe71ba219efba0bc6b0d219a6b3acf
Jelger Haanstra <jghaanstra@gmail.com>
2021-11-13 19:32:45 +0100

    Release 3.4.10

--- drivers/device.js
+++ drivers/device.js
@@ -662,7 +662,7 @@ class ShellyDevice extends Homey.Device {
         case 'power3':
           this.updateCapabilityValue('measure_power', value, channel);
           break;
-        case: 'total':
+        case 'total':
         case 'energyCounter0':
         case 'energyCounter1':
         case 'energyCounter2':
