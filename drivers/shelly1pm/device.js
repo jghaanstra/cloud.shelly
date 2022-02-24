1d925ecd2c74455749acb6e058325d7962f1d185
Jelger Haanstra <jghaanstra@gmail.com>
2022-02-24 17:38:57 +0100

    Release 3.7.3

--- drivers/shelly1pm/device.js
+++ drivers/shelly1pm/device.js
@@ -9,7 +9,10 @@ class Shelly1pmDevice extends Device {
   onInit() {
     if (!this.util) this.util = new Util({homey: this.homey});
 
-    this.callbacks = [];
+    this.callbacks = [
+      'shortpush',
+      'longpush'
+    ];
 
     this.homey.flow.getDeviceTriggerCard('triggerInput1On');
     this.homey.flow.getDeviceTriggerCard('triggerInput1Off');
