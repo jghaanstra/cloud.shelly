181d796862ad18af8a6d68edd3c74000eb020882
Jelger Haanstra <jghaanstra@gmail.com>
2022-08-29 09:12:31 +0200

    Release 3.11.2

--- drivers/driver.js
+++ drivers/driver.js
@@ -25,31 +25,36 @@ class ShellyDriver extends Homey.Driver {
 
         /* fill devices object with discovered devices */
         const devices = Object.values(discoveryResults).map(discoveryResult => {
-          let needsPairing = false;
 
-          /* match discovery result with already paired Shellies to determine if there are unpaired devices */
-          if (shellyDevices.length === 0) { // no paired shellies
-            unpairedShellies++
-            needsPairing = true;
-          } else if (shellyDevices.length > 0) {
-            const pairedShelly = shellyDevices.filter(shelly => shelly.id.includes(discoveryResult.host));
-            if (pairedShelly.length === 0) { // paired shellies available but this ID has not been paired yet
+          if (discoveryResult === undefined || discoveryResult === null) {
+            this.error('discoveryResult is not defined, logging discoveryResults object.');
+            this.error(JSON.stringify(discoveryResults));
+          } else {
+            let needsPairing = false;
+
+            /* match discovery result with already paired Shellies to determine if there are unpaired devices */
+            if (shellyDevices.length === 0) { // no paired shellies
               unpairedShellies++
               needsPairing = true;
-            }
-          }
-
-          
-          if (needsPairing) {
-            return {
-              name: discoveryResult.host + ' ['+ discoveryResult.address +']',
-              data: {
-                id: discoveryResult.host
+            } else if (shellyDevices.length > 0) {
+              const pairedShelly = shellyDevices.filter(shelly => shelly.id.includes(discoveryResult.host));
+              if (pairedShelly.length === 0) { // paired shellies available but this ID has not been paired yet
+                unpairedShellies++
+                needsPairing = true;
               }
-            };
-          } else { // do not return already paired devices
-            return {};
-          }          
+            }
+
+            if (needsPairing) {
+              return {
+                name: discoveryResult.host + ' ['+ discoveryResult.address +']',
+                data: {
+                  id: discoveryResult.host
+                }
+              };
+            } else { // do not return already paired devices
+              return {};
+            } 
+          }      
 
         });
 
@@ -73,88 +78,96 @@ class ShellyDriver extends Homey.Driver {
       try {
         const discoveryResult = discoveryResults[selectedDeviceId];
 
-        /* get device config based on hostname of the discovered device */
-        const hostname = discoveryResult.host.substr(0, discoveryResult.host.lastIndexOf("-") + 1);
-        let device_config = this.util.getDeviceConfig(hostname);
-
-        if (typeof device_config === 'undefined') {
-          this.log('No device config found for device with hostname', hostname);
-          throw new Error(this.homey.__('pair.no_device_config') + ' Device has hostname:' + hostname);
-        }
-
-        switch(device_config.gen) {
-          case 'gen1':
-            var result = await this.util.sendCommand('/shelly', discoveryResult.address, '', '');
-            var auth = result.auth;
-            var type = result.type;
-
-            /* update device config if it's a roller shutter */
-            if (result.hasOwnProperty("num_rollers") && result.hasOwnProperty("mode")) {
-              if (result.mode === 'roller') {
-                device_config = this.util.getDeviceConfig(hostname + 'roller-');
-              }
-            } else if (result.hasOwnProperty("num_rollers")) {
-              // fallback for devices with fw < 1.12 but will not work with authentication
-              let settings = await this.util.sendCommand('/settings', discoveryResult.address, '', '');
-              if (settings.mode === 'roller') {
-                device_config = this.util.getDeviceConfig(hostname + 'roller-');
-              }
-            }
-
-            /* update device config if it's a RGBW2 in white mode */
-            if (device_config.name === 'Shelly RGBW2 Color') {
-              if (result.num_outputs === 4) {
-                device_config = this.util.getDeviceConfig(hostname + 'white-');
-              }
-            }
-
-            break;
-          case 'gen2':
-            var result = await this.util.sendCommand('/rpc/Shelly.GetDeviceInfo', discoveryResult.address, '', '');
-            var auth = result.auth_en;
-            var type = result.model;
-
-            /* update device config if it's a roller shutter */
-            if (result.hasOwnProperty("profile")) {
-              if (result.profile === "cover") {
-                device_config = this.util.getDeviceConfig(hostname + 'roller-');
-              }
-            }
-            break;
-        }
-
-        deviceArray = {
-          name: device_config.name + ' ['+ discoveryResult.address +']',
-          class: device_config.class,
-          data: {
-            id: discoveryResult.host,
-          },
-          settings: {
-            address  : discoveryResult.address,
-            username : '',
-            password : ''
-          },
-          capabilities: device_config.capabilities_1,
-          capabilitiesOptions: device_config.capability_options,
-          energy: device_config.energy,
-          store: {
-            config: device_config,
-            main_device: discoveryResult.host,
-            channel: 0,
-            type: type,
-            unicast: false,
-            wsserver: false,
-            battery: device_config.battery,
-            sdk: 3,
-            gen: device_config.gen,
-            communication: device_config.communication
-          },
-          icon: device_config.icon
-        }
-        if (auth) {
-          session.showView('login_credentials');
+        if (discoveryResult === undefined || discoveryResult === null) {
+          this.error('selected device from discoveryResults is not defined, logging discoveryResults object and selectedDeviceId.');
+          this.error('selectedDeviceId:', selectedDeviceId);
+          this.error(JSON.stringify(discoveryResults));
+          throw new Error('Selected device from discoveryResults is undefined, please send a diagnostic report to the developer.')
         } else {
-          session.showView('add_device');
+
+          /* get device config based on hostname of the discovered device */
+          const hostname = discoveryResult.host.substr(0, discoveryResult.host.lastIndexOf("-") + 1);
+          let device_config = this.util.getDeviceConfig(hostname);
+
+          if (typeof device_config === 'undefined') {
+            this.log('No device config found for device with hostname', hostname);
+            throw new Error(this.homey.__('pair.no_device_config') + ' Device has hostname:' + hostname);
+          }
+
+          switch(device_config.gen) {
+            case 'gen1':
+              var result = await this.util.sendCommand('/shelly', discoveryResult.address, '', '');
+              var auth = result.auth;
+              var type = result.type;
+
+              /* update device config if it's a roller shutter */
+              if (result.hasOwnProperty("num_rollers") && result.hasOwnProperty("mode")) {
+                if (result.mode === 'roller') {
+                  device_config = this.util.getDeviceConfig(hostname + 'roller-');
+                }
+              } else if (result.hasOwnProperty("num_rollers")) {
+                // fallback for devices with fw < 1.12 but will not work with authentication
+                let settings = await this.util.sendCommand('/settings', discoveryResult.address, '', '');
+                if (settings.mode === 'roller') {
+                  device_config = this.util.getDeviceConfig(hostname + 'roller-');
+                }
+              }
+
+              /* update device config if it's a RGBW2 in white mode */
+              if (device_config.name === 'Shelly RGBW2 Color') {
+                if (result.num_outputs === 4) {
+                  device_config = this.util.getDeviceConfig(hostname + 'white-');
+                }
+              }
+
+              break;
+            case 'gen2':
+              var result = await this.util.sendCommand('/rpc/Shelly.GetDeviceInfo', discoveryResult.address, '', '');
+              var auth = result.auth_en;
+              var type = result.model;
+
+              /* update device config if it's a roller shutter */
+              if (result.hasOwnProperty("profile")) {
+                if (result.profile === "cover") {
+                  device_config = this.util.getDeviceConfig(hostname + 'roller-');
+                }
+              }
+              break;
+          }
+
+          deviceArray = {
+            name: device_config.name + ' ['+ discoveryResult.address +']',
+            class: device_config.class,
+            data: {
+              id: discoveryResult.host,
+            },
+            settings: {
+              address  : discoveryResult.address,
+              username : '',
+              password : ''
+            },
+            capabilities: device_config.capabilities_1,
+            capabilitiesOptions: device_config.capability_options,
+            energy: device_config.energy,
+            store: {
+              config: device_config,
+              main_device: discoveryResult.host,
+              channel: 0,
+              type: type,
+              unicast: false,
+              wsserver: false,
+              battery: device_config.battery,
+              sdk: 3,
+              gen: device_config.gen,
+              communication: device_config.communication
+            },
+            icon: device_config.icon
+          }
+          if (auth) {
+            session.showView('login_credentials');
+          } else {
+            session.showView('add_device');
+          }
         }
       } catch (error) {
         return Promise.reject(error);
